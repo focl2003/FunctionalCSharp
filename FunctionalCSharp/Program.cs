0b348866229b17293ca83595a7b194b7c19a563f
Carlos Alberto <focl2003@yahoo.com>
2018-09-07 11:28:51 -0500

    Agregar archivos de proyecto.

---
+++ FunctionalCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Unit = System.ValueTuple;
+
+namespace FunctionalCSharp {
+   
+
+   public static class Program {
+      static void Main(string[] args) {
+         Int32[] numeros = { 1 ,2, 3, 4, 5};
+         Console.WriteLine(numeros.ToString());
+         Console.ReadLine();
+      }
+
+      public static String GetName(String name) {
+         return name;
+      }
+   }
+
+}
